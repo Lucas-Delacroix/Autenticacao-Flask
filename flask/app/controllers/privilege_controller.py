016e324931c6f15222b421330d996168fcff8d10
Lucas Delacroix <lucas.delacroix.alves08@aluno.ifce.edu.br>
2022-04-12 10:31:54 -0300

    CRUD da tabela Privileges completo

--- flask/app/controllers/privilege_controller.py
+++ flask/app/controllers/privilege_controller.py
@@ -37,60 +37,73 @@ def privilege_list():
     return jsonify({"elements": arr, "error": False})
 
 
-@app.route("/privilege/edit/<int:role_id>-<int:resource_id>-<string:allow>", methods=["PUT"])
+@app.route("/privilege/edit/<int:role_id>/<int:resource_id>/<string:allow>", methods=["PUT"])
 def privilege_edit(role_id, resource_id, allow):
     data = request.get_json()
-    privilege = Privilege.query.get(1)
+    privileges = Privilege.query.filter_by(role_id=role_id, resource_id=resource_id, allow=allow)
 
-    if privilege == None:
+    if privileges == None:
         return jsonify({"error": True, "message": "O privilege informado não existe."})
+    
+    arr = []
+    for privilege in privileges:
+        arr.append(privilege.to_dict()) 
 
     try:
         if "role_id" in data:
-            user.role_id = data["role_id"]
+            privilege.role_id = data["role_id"]
         
         if "resource_id" in data:
-            user.resource_id = data["resource_id"]
+            privilege.resource_id = data["resource_id"]
         
         if "allow" in data:
-            user.allow = data["allow"]
+            privilege.allow = data["allow"]
 
         db.session.commit()
-        return jsonify({"error": False, "message": "privilege editada com sucesso."})
+        return jsonify({"error": False, "message": "Privilege editada com sucesso."})
 
     except:
         db.session.rollback()
         return jsonify({"error": True, "message": "Erro ao editar privilege."}), 200
 
 
-@app.route("/privilege/delete/<int:id>", methods=["DELETE"])
-def privilege_delete(id):
-    privilege = Privilege.query.get(id)
+@app.route("/privilege/delete/<int:role_id>/<int:resource_id>/<string:allow>", methods=["DELETE"])
+def privilege_delete(role_id, resource_id, allow):
+    privileges = Privilege.query.filter_by(role_id=role_id, resource_id=resource_id, allow=allow)
 
-    if privilege == None:
+    if privileges == None:
         return jsonify({"error": True, "message": "A privilege informada não existe."})
+    
+    arr = []
+    for privilege in privileges:
+        arr.append(privilege.to_dict()) 
 
     try:
         db.session.delete(privilege)
         db.session.commit()
-        return jsonify({"error": False, "message": "privilege deletado com sucesso."})
+        return jsonify({"error": False, "message": "Privilege deletado com sucesso."})
 
     except:
         db.session.rollback()
         return jsonify({"error": True, "message": "Erro ao deletar privilege."}), 200
 
 
-@app.route("/privilege/view/<int:role_id>", methods=["GET"])
-def privilege_view(role_id):
-    privilege = Privilege.query.get(role_id)
-
-    if privilege == None:
+@app.route("/privilege/view/<int:role_id>/<int:resource_id>/<string:allow>", methods=["GET"])
+def privilege_view(role_id, resource_id, allow):
+    privileges = Privilege.query.filter_by(role_id=role_id, resource_id=resource_id, allow=allow)
+    
+    if privileges == None:
         return jsonify({"error": True, "message": "O privilege informada não existe."})
+    
+    arr = []
+    for privilege in privileges:
+        arr.append(privilege.to_dict()) 
+
 
     try:
         return jsonify({"data": privilege.to_dict(), "error": False})
 
     except:
         db.session.rollback()
-        return jsonify({"error": True, "message": "Erro ao visualizar privilege."}), 200
+        return jsonify({"error": True, "message": "Erro ao visualizar privilege. Verifique se as informações inseridas estão corretas."}), 200
 
